9c9aefda0fcf0e013722ded85ea9cb744cd0a3b4
01ghostboy10 <levana.rai.simcik@mensa.cz>
2026-07-06 18:20:00 +0800

    IT FINALLY WORKS YAYYYY

--- firmware/soupInABag/soupInABag.ino
+++ firmware/soupInABag/soupInABag.ino
@@ -1,15 +1,16 @@
 #include <Servo.h>
-#include <vector>
 
-int VSENS = D0;
+
+int VSENS = D1;
 
 
 // int currentNUMBER = 0;
 // int startNum = 0;
 bool isJumping = false;
 // long startTime = millis(); // track time
+int jumpSensitivity = 1000;
+int failedCheckThreshold = 125;
 
-std::vector<int> jumpList;
 
 int lastTimeChecked;
 int timeElapsed;
@@ -26,7 +27,7 @@ void loop()
 {
   ElapseTime();
   jumpCheckLoop();
-  // cactusMoveLoop();
+  cactusMoveLoop();
   // sensorIsOn();
 }
 
@@ -52,37 +53,39 @@ void jumpCheckLoop(){
   long vsenstime;
 
   // vSensCheck();
-  // JumpCheck();
+  JumpCheck();
+  // Serial.println(digitalRead(VSENS));
 
   // Serial.println(digitalRead(VSENS));
   if(digitalRead(VSENS))
     jumpMilSecs++;
   else if(jumpMilSecs != 0)
   {
-    if(jumpMilSecs >= 50000)
+    if(jumpMilSecs >= jumpSensitivity)
     {
-      Serial.print(failedChecks);
-      Serial.print("    ");
-      Serial.println(jumpMilSecs);
+      // Serial.print(failedChecks);
+      // Serial.print("    ");
+      // Serial.println(jumpMilSecs);
+      // Serial.print("    ");
       failedChecks = 0;
     }
     else
       failedChecks++;
-    jumpList.push_back(jumpMilSecs);
     jumpMilSecs = 0;
   }
 }
 // - - - - - - - \*
-// void JumpCheck() {
-//   if (jumpMilSecs > 100) {
-//     if ((jumpList[1]) > 200) {
-//       isJumping == true;
+void JumpCheck() {
+  if (jumpMilSecs > jumpSensitivity) {
+    if (failedChecks <= failedCheckThreshold) {
+      isJumping == true;
 
       // Serial.print(random(2, 500));
-      // Serial.println("PLAYER IS JUMPINGG");
-//     }
-//   }
-// }
+      Serial.println("PLAYER IS JUMPINGG");
+      // Serial.println(jumpMilSecs);
+    }
+  }
+}
 
 #pragma endregion jumpCodings
 /* - - - - - - - - - - - - - - - - - // - - - - - - - - - - - - - - - - - */
@@ -173,110 +176,4 @@ bool delayCactusStepForSecs(String step, int timeToStall, String nextStep)
 
 #pragma endregion cactusControls
 
-
-// void sensorIsOn() {
-//   if (digitalRead(VSENS) == HIGH) {
-//     Serial.println("The sensor is on!");
-//     delay(100);
-
-
-
-
-
-
-/* - - - - - - - - - - - - - - - - - // - - - - - - - - - - - - - - - - - */
-// void JumpCheck() {
-//   if (jumpMilSecs > 200) {
-//     if (((jumpList[jumpMilSecs + 1]) && (jumpList[jumpMilSecs + 2]) && (jumpList[jumpMilSecs + 3]) && (jumpList[jumpMilSecs + 4])) > 200) {
-//       isJumping == true;
-//       Serial.println("PLAYER IS JUMPINGG");
-//     }
-//   }
-// }
-
-/* - - - - - - - - - - - - - - - - - // - - - - - - - - - - - - - - - - - */
-  //
-
-
-                    // #include <Wire.h>
-                    // #include <Adafruit_GFX.h>
-                    // // #include <Adafruit_SSD1306.h>
-
-                    // #define MPU_ADDR 0x68
-
-                    // float rotationZ = 0;
-                    // unsigned long lastTime = 0;
-
-
-                    // void setup() {
-                    // Serial.begin(9600);
-                    //   Wire.begin();
-
-                    //   // display.clearDisplay();
-                    //   // display.setTextSize(1);
-                    //   // display.setTextColor(SSD1306_WHITE);
-                    //   delay(100);
-
-                    //   initMPU();
-                    // }
-
-                    // void loop() {
-                    //   readGyro();
-
-                    //   displayRot();
-
-                    //   delay(200);
-                    // }
-
-                    // void initMPU() {
-                    //   // Wake up MPU
-                    //   Wire.beginTransmission(MPU_ADDR);
-                    //   Wire.write(0x6B);
-                    //   Wire.write(0x00);
-                    //   Wire.endTransmission();
-
-                    //   // Set gyro range to ±250°/s
-                    //   Wire.beginTransmission(MPU_ADDR);
-                    //   Wire.write(0x1B);
-                    //   Wire.write(0x00);
-                    //   Wire.endTransmission();
-                    // }
-
-                    // void readGyro() {
-                    //   int16_t gyroX, gyroY, gyroZ;
-                    //   Wire.beginTransmission(MPU_ADDR);
-                    //   Wire.write(0x43);  //GYRO_XOUT_H
-                    //   Wire.endTransmission(false);
-
-                    //   //request 6 bytes (1 per register)
-                    //   Wire.requestFrom(MPU_ADDR, 6);
-
-                    //   gyroX = (Wire.read() << 8) | Wire.read();
-                    //   gyroY = (Wire.read() << 8) | Wire.read();
-                    //   gyroZ = (Wire.read() << 8) | Wire.read();
-
-                    //   //converting to degrees/second
-                    //   float gyroRate = gyroZ / 131.0;
-
-                    //   //calculating time elapsed
-                    //   unsigned long now = millis();
-                    //   float dt = (now - lastTime) / 1000.0;
-
-                    //   //math to get total rotation
-                    //   rotationZ += gyroRate * dt;
-                    //   lastTime = now;
-                    // }
-
-                    // void displayRot() {
-                    //   // display.clearDisplay();
-                    //   // display.setCursor(0, 0);
-                    //   // display.print("Current Rotation:");
-                    //   // display.setCursor(0, 16);
-                    //   // display.print(rotationZ);
-                    // Serial.println(rotationZ);
-                    // }
-                    // }
-                    // }
-                    // }
-                    // }
                     // }
